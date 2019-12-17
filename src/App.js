443fa7a2b48a47dde00367b5f5514b36ffd06d17
Oleksii <56698837+oleksii-suvorov@users.noreply.github.com>
2019-12-17 01:49:00 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import Users from './components/Users';
-import './style.css'
 
 export default class App extends React.Component {
     render(){
